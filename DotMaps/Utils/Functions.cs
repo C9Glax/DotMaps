aff4ee2df710c797f2c914a73664dde6a297e93f
Glax <gfreak70@gmail.com>
2020-03-27 19:49:31 +0100

    Code Cleanup

---
+++ DotMaps/Utils/Functions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DotMaps.Utils
+{
+    public class Functions
+    {
+        public static float CalculateDistanceBetweenCoordinates(float lat1, float lon1, float lat2, float lon2)
+        {
+            int earthRadius = 6371;
+            float differenceLat = DegreesToRadians(lat2 - lat1);
+            float differenceLon = DegreesToRadians(lon2 - lon1);
+
+            float lat1Rads = DegreesToRadians(lat1);
+            float lat2Rads = DegreesToRadians(lat2);
+
+            double a = Math.Sin(differenceLat / 2) * Math.Sin(differenceLat / 2) + Math.Sin(differenceLon / 2) * Math.Sin(differenceLon / 2) * Math.Cos(lat1Rads) * Math.Cos(lat2Rads);
+            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+            return Convert.ToSingle(earthRadius * c);
+        }
+
+        public static float DegreesToRadians(float degrees)
+        {
+            return degrees * Convert.ToSingle(Math.PI) / 180;
+        }
+    }
+}
---
+++ DotMaps/Utils/Importer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using DotMaps.Datastructures;
+using DotMaps.Utils;
+
+namespace Utils
+{
+    public class Importer
+    {
+        public static Graph ReadNodesIntoNewGraph(Hashtable nodes)
+        {
+            Graph map = new Graph();
+            return ReadNodesIntoGraph(nodes, map);
+        }
+
+        public static Graph ReadNodesIntoGraph(Hashtable nodes, Graph graph)
+        {
+            foreach (Node node in nodes.Values)
+                graph.nodes.Add(node.id, node);
+            return graph;
+        }
+
+        public static void ReadWaysIntoGraph(List<Way> ways, ref Graph graph)
+        {
+            Hashtable speeds = new Hashtable();
+            foreach (string speed in System.IO.File.ReadAllLines("speeds.txt"))
+                speeds.Add(speed.Split(',')[0], Convert.ToInt32(speed.Split(',')[1]));
+
+            foreach (Way way in ways)
+            {
+                UInt64[] nodeIds = way.nodes.ToArray();
+                for (int i = 1; i < nodeIds.Length; i++)
+                {
+                    Node fromNode = (Node)graph.nodes[nodeIds[i]];
+                    Node toNode = (Node)graph.nodes[nodeIds[i - 1]];
+
+                    double distance = Functions.CalculateDistanceBetweenCoordinates(fromNode.lat, fromNode.lon, toNode.lat, toNode.lon);
+                    int speed = 50;
+                    if (speeds.ContainsKey((string)way.tags["highway"]))
+                        speed = (int)way.tags["highway"];
+                    if (way.tags.ContainsKey("maxspeed"))
+                        try
+                        {
+                            speed = Convert.ToInt32((string)way.tags["maxspeed"]);
+                        }
+                        catch (FormatException)
+                        {
+                            Console.WriteLine("Maxspeed {0} not implemented", (string)way.tags["maxspeed"]);
+                        }
+                    float timeNeeded = (float)distance / speed;
+
+                    string name = "";
+                    if (way.tags.ContainsKey("ref"))
+                        name = (string)way.tags["ref"];
+                    else if (way.tags.ContainsKey("name"))
+                        name = (string)way.tags["name"];
+
+                    toNode.AddConnection(new Connection(distance, timeNeeded, fromNode, (string)way.tags["highway"], name));
+                    if (!way.tags.ContainsKey("oneway") || ((string)way.tags["oneway"]) == "no")
+                        fromNode.AddConnection(new Connection(distance, timeNeeded, toNode, (string)way.tags["highway"], name));
+                }
+            }
+        }
+    }
+}
