769a52c8025fade7a829450a1bf03e3adaa981c4
Glax <gfreak70@gmail.com>
2020-03-31 02:29:45 +0200

    Expanded Functionality

--- DotMaps/Utils/Functions.cs
+++ DotMaps/Utils/Functions.cs
@@ -1,47 +1,130 @@
 ﻿using System;
+using DotMaps.Datastructures;
 
 namespace DotMaps.Utils
 {
     public class Functions
     {
-        public static float CalculateDistanceBetweenCoordinates(float lat1, float lon1, float lat2, float lon2)
+        const int earthRadius = 6371;
+        public static double DistanceBetweenNodes(_3DNode node1, _3DNode node2)
         {
-            int earthRadius = 6371;
-            float differenceLat = DegreesToRadians(lat2 - lat1);
-            float differenceLon = DegreesToRadians(lon2 - lon1);
+            return DistanceBetweenCoordinates(node1.lat, node1.lon, node2.lat, node2.lon);
+        }
 
-            float lat1Rads = DegreesToRadians(lat1);
-            float lat2Rads = DegreesToRadians(lat2);
+        public static double DistanceBetweenCoordinates(float lat1, float lon1, float lat2, float lon2)
+        {
+            double differenceLat = DegreesToRadians(lat2 - lat1);
+            double differenceLon = DegreesToRadians(lon2 - lon1);
+
+            double lat1Rads = DegreesToRadians(lat1);
+            double lat2Rads = DegreesToRadians(lat2);
 
             double a = Math.Sin(differenceLat / 2) * Math.Sin(differenceLat / 2) + Math.Sin(differenceLon / 2) * Math.Sin(differenceLon / 2) * Math.Cos(lat1Rads) * Math.Cos(lat2Rads);
             double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
 
-            return Convert.ToSingle(earthRadius * c);
+            return earthRadius * c;
         }
 
-        public static float DegreesToRadians(float degrees)
+        public static double DegreesToRadians(float degrees)
         {
-            return degrees * Convert.ToSingle(Math.PI) / 180;
+            return degrees * Math.PI / 180;
         }
-
-        public static double AngleBetweenCoordinates(float lat1, float lon1, float lat2, float lon2)
-        {
-            lat1 = DegreesToRadians(lat1);
-            lon1 = DegreesToRadians(lon1);
-            lat2 = DegreesToRadians(lat2);
-            lon2 = DegreesToRadians(lon2);
-
-            var y = Math.Sin(lon2 - lon1) * Math.Cos(lat2);
-            var x = Math.Cos(lat1) * Math.Sin(lat2) -
-                    Math.Sin(lat1) * Math.Cos(lat2) * Math.Cos(lon2 - lon1);
-            var angle = Math.Atan2(y, x);
-
-            return angle;
-        }
-
         public static double RadiansToDegrees(double Radians)
         {
             return Radians * 180 / Math.PI;
         }
+
+        public static double AngleBetweenNodes(_3DNode node1, _3DNode node2)
+        {
+            return AngleBetweenCoordinates(node1.lat, node1.lon, node2.lat, node2.lon);
+        }
+
+        public static double AngleBetweenCoordinates(float lat1, float lon1, float lat2, float lon2)
+        {
+            double lt1 = DegreesToRadians(lat1);
+            double ln1 = DegreesToRadians(lon1);
+            double lt2 = DegreesToRadians(lat2);
+            double ln2 = DegreesToRadians(lon2);
+
+            double y = Math.Sin(ln2 - ln1) * Math.Cos(lt2);
+            double x = Math.Cos(lt1) * Math.Sin(lt2) - Math.Sin(lt1) * Math.Cos(lt2) * Math.Cos(ln2 - ln1);
+            double angle = Math.Atan2(y, x);
+
+            return angle;
+        }
+
+        public static _2DNode _2DNodeFrom3DNode(_3DNode node, _3DNode center, int resolution)
+        {
+            double ax = earthRadius * Math.Cos(DegreesToRadians(node.lat)) * Math.Cos(DegreesToRadians(node.lon)); //sphere
+            double ay = earthRadius * Math.Cos(DegreesToRadians(node.lat)) * Math.Sin(DegreesToRadians(node.lon)); //sphere
+            double az = earthRadius * Math.Sin(DegreesToRadians(node.lat)); //sphere
+
+            double cx = 0;// earthRadius * (resolution + 1) * Math.Cos(DegreesToRadians(center.lat)) * Math.Cos(DegreesToRadians(center.lon)); //camera
+            double cy = 0;// earthRadius * (resolution + 1) * Math.Cos(DegreesToRadians(center.lat)) * Math.Sin(DegreesToRadians(center.lon)); //camera
+            double cz = 0;// earthRadius * (resolution + 1) * Math.Sin(DegreesToRadians(center.lat)); //camera
+
+            double ox = 0; //cameraangle
+            double oy = DegreesToRadians(node.lon);//cameraangle
+            double oz = DegreesToRadians(node.lat);//cameraangle
+
+            double ex = 0; //screenrelativetopinhole
+            double ey = 0; //screenrelativetopinhole
+            double ez = earthRadius * resolution; //screenrelativetopinhole
+
+            double x = ax - cx;
+            double y = ay - cy;
+            double z = az - cz;
+
+            double dx = Math.Cos(oy) * (Math.Sin(oz) * y + Math.Cos(oz) * x) - Math.Cos(oy) * z;
+            double dy = Math.Sin(ox) * (Math.Cos(oy) * z + Math.Sin(oy) * (Math.Sin(oy) * y + Math.Cos(oz) * x)) + Math.Cos(ox) * (Math.Cos(oz) * y - Math.Sin(oz) * x);
+            double dz = Math.Cos(ox) * (Math.Cos(oy) * z + Math.Sin(oy) * (Math.Sin(oz) * y + Math.Cos(oz) * x)) - Math.Sin(ox) * (Math.Cos(oz) * y - Math.Sin(oz) * x);
+
+            double bx = ez / dz * dx + ex;
+            double by = ez / dz * dy + ey;
+
+            return new _2DNode(node.id, (float)-bx, (float)by);
+        }
+
+
+        /*
+        public static int[] GridFromCoordinates(float lengthCellLateralEdgeClosestToEquator, float startLat, float startLon, float finishLat, float finishLon) {
+            double angle = AngleBetweenCoordinates(startLat, startLon, finishLat, finishLon);
+            double distance = CalculateDistanceBetweenCoordinates(startLat, startLon, finishLat, finishLon);
+            int x = (int)(Math.Sin(angle) * distance / lengthCellLateralEdgeClosestToEquator);
+            int y = (int)-(Math.Cos(angle) * distance / lengthCellLateralEdgeClosestToEquator);
+            return new int[2] { x, y };
+        }
+
+        public static float[] PixelsFromCoordinates(int resolution, float maxLat, float minLon, float nodeLat, float nodeLon)
+        {
+            double angle = AngleBetweenCoordinates(maxLat, minLon, nodeLat, nodeLon);
+            double distance = CalculateDistanceBetweenCoordinates(maxLat, minLon, nodeLat, nodeLon);
+            float pixelX = (float)(Math.Sin(angle) * distance) * resolution/10;
+            float pixelY = (float)-(Math.Cos(angle) * distance) * resolution/10;
+            return new float[2] { pixelX, pixelY };
+        }
+
+        public static float[] GridCellMaxLatMinLon(float lengthCellLateralEdgeClosestToEquator, float gridMinLat, float gridMaxLat, float gridMinLon, int gridX, int gridY)
+        {
+            const float kmAtEquator = 111.699f;
+            float cellMaxLat = gridMaxLat - (lengthCellLateralEdgeClosestToEquator / kmAtEquator) * gridY;
+            float cellMinLon = gridMinLon + (float)Math.Cos(gridMinLat) * (lengthCellLateralEdgeClosestToEquator / kmAtEquator) * gridX;
+            return new float[2] { cellMaxLat, cellMinLon };
+        }
+
+        public static List<Line>[,] EmptyGridFromBounds(float lengthCellLateralEdgeClosestToEquator, float minLat, float maxLat, float minLon, float maxLon)
+        {
+            double width = CalculateDistanceBetweenCoordinates(minLat, minLon, minLat, maxLon);
+            double height = CalculateDistanceBetweenCoordinates(minLat, minLon, maxLat, minLon);
+            int amountX = (int)Math.Ceiling(width / lengthCellLateralEdgeClosestToEquator);
+            int amountY = (int)Math.Ceiling(height / lengthCellLateralEdgeClosestToEquator);
+
+            List<Line>[,] emptyGrid = new List<Line>[amountX, amountY];
+            for (int x = 0; x < emptyGrid.GetLength(0); x++)
+                for (int y = 0; y < emptyGrid.GetLength(1); y++)
+                    emptyGrid[x, y] = new List<Line>();
+
+            return emptyGrid;
+        }*/
     }
 }
--- DotMaps/Utils/Importer.cs
+++ DotMaps/Utils/Importer.cs
@@ -1,12 +1,145 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections;
 using DotMaps.Datastructures;
+using System.Xml;
 
 namespace DotMaps.Utils
 {
     public class Importer
     {
+
+        public static Graph ReadNodesFromOSMFileIntoGraph(string path)
+        {
+            Graph graph = new Graph();
+
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.IgnoreWhitespace = true;
+            using (XmlReader reader = XmlReader.Create(path, settings)) {
+                while (reader.Read())
+                {
+                    if (reader.NodeType != XmlNodeType.EndElement)
+                    {
+                        if (reader.Depth == 1)
+                        {
+                            switch (reader.Name)
+                            {
+                                case "node":
+                                    UInt64 id = Convert.ToUInt64(reader.GetAttribute("id"));
+                                    float lat = Convert.ToSingle(reader.GetAttribute("lat").Replace('.', ','));
+                                    float lon = Convert.ToSingle(reader.GetAttribute("lon").Replace('.', ','));
+                                    graph.nodes.Add(id, new _3DNode(id, lat, lon));
+                                    break;
+                                case "bounds":
+                                    graph.minLat = Convert.ToSingle(reader.GetAttribute("minlat").Replace('.', ','));
+                                    graph.minLon = Convert.ToSingle(reader.GetAttribute("minlon").Replace('.', ','));
+                                    graph.maxLat = Convert.ToSingle(reader.GetAttribute("maxlat").Replace('.', ','));
+                                    graph.maxLon = Convert.ToSingle(reader.GetAttribute("maxlon").Replace('.', ','));
+                                    break;
+                            }
+                        }
+                    }
+                }
+                reader.Close();
+            }
+            return graph;
+        }
+
+        public static void  ReadWaysFromOSMFileIntoConnections(string path, ref Graph graph)
+        {
+            Hashtable speeds = new Hashtable();
+            foreach (string speed in System.IO.File.ReadAllLines("speeds.txt"))
+                speeds.Add(speed.Split(',')[0], Convert.ToInt32(speed.Split(',')[1]));
+
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.IgnoreWhitespace = true;
+            const byte UNKNOWN = 0, NODE = 1, WAY = 2, READING = 1, DONE = 0;
+            byte nodeType = UNKNOWN, state = DONE;
+
+            Way currentWay = new Way(0);
+            using (XmlReader reader = XmlReader.Create(path, settings))
+            {
+                while (reader.Read())
+                {
+                    if (reader.NodeType != XmlNodeType.EndElement)
+                    {
+                        if (reader.Depth == 1)
+                        {
+                            if (state == READING)
+                            {
+                                state = DONE;
+                                if (nodeType == WAY)
+                                {
+                                    if (currentWay.tags.ContainsKey("highway"))
+                                    {
+                                        for (int i = 1; i < currentWay.nodes.Count; i++)
+                                        {
+                                            _3DNode fromNode = currentWay.nodes[i];
+                                            _3DNode toNode = currentWay.nodes[i - 1];
+
+                                            double distance = Functions.DistanceBetweenCoordinates(fromNode.lat, fromNode.lon, toNode.lat, toNode.lon);
+                                            int speed = 50;
+                                            if (currentWay.tags.ContainsKey("highway") && speeds.ContainsKey((string)currentWay.tags["highway"]))
+                                                speed = (int)speeds[(string)currentWay.tags["highway"]];
+                                            if (currentWay.tags.ContainsKey("maxspeed"))
+                                                try
+                                                {
+                                                    speed = Convert.ToInt32((string)currentWay.tags["maxspeed"]);
+                                                }
+                                                catch (FormatException)
+                                                {
+                                                    Console.WriteLine("Maxspeed {0} not implemented", (string)currentWay.tags["maxspeed"]);
+                                                }
+                                            float timeNeeded = (float)distance / speed;
+
+                                            string name = "";
+                                            if (currentWay.tags.ContainsKey("ref"))
+                                                name = (string)currentWay.tags["ref"];
+                                            else if (currentWay.tags.ContainsKey("name"))
+                                                name = (string)currentWay.tags["name"];
+
+                                            toNode.AddConnection(new Connection(distance, timeNeeded, fromNode, (string)currentWay.tags["highway"], name));
+                                            if (!currentWay.tags.ContainsKey("oneway") || ((string)currentWay.tags["oneway"]) == "no")
+                                                fromNode.AddConnection(new Connection(distance, timeNeeded, toNode, (string)currentWay.tags["highway"], name));
+                                        }
+                                    }
+                                }
+                            }
+                            switch (reader.Name)
+                            {
+                                case "node":
+                                    nodeType = NODE;
+                                    break;
+                                case "way":
+                                    currentWay = new Way(Convert.ToUInt64(reader.GetAttribute("id")));
+                                    nodeType = WAY;
+                                    break;
+                                default:
+                                    nodeType = UNKNOWN;
+                                    break;
+                            }
+                        }
+                        else if (reader.Depth == 2 && nodeType == WAY)
+                        {
+                            state = READING;
+                            switch (reader.Name)
+                            {
+                                case "nd":
+                                    UInt64 id = Convert.ToUInt64(reader.GetAttribute("ref"));
+                                    currentWay.nodes.Add((_3DNode)graph.nodes[id]);
+                                    break;
+                                case "tag":
+                                    string key = reader.GetAttribute("k");
+                                    if (key.StartsWith("addr:") || key == "building" || key == "ref" || key == "highway" || key == "name" || key == "maxspeed" || key == "oneway")
+                                        currentWay.tags.Add(key, reader.GetAttribute("v"));
+                                    break;
+                            }
+                        }
+                    }
+                }
+                reader.Close();
+            }
+        }
+
         public static Graph ReadNodesIntoNewGraph(Hashtable nodes)
         {
             Graph map = new Graph();
@@ -15,51 +148,9 @@ namespace DotMaps.Utils
 
         public static Graph ReadNodesIntoGraph(Hashtable nodes, Graph graph)
         {
-            foreach (Node node in nodes.Values)
+            foreach (_3DNode node in nodes.Values)
                 graph.nodes.Add(node.id, node);
             return graph;
-        }
-
-        public static void ReadWaysIntoGraph(List<Way> ways, ref Graph graph)
-        {
-            Hashtable speeds = new Hashtable();
-            foreach (string speed in System.IO.File.ReadAllLines("speeds.txt"))
-                speeds.Add(speed.Split(',')[0], Convert.ToInt32(speed.Split(',')[1]));
-
-            foreach (Way way in ways)
-            {
-                UInt64[] nodeIds = way.nodes.ToArray();
-                for (int i = 1; i < nodeIds.Length; i++)
-                {
-                    Node fromNode = (Node)graph.nodes[nodeIds[i]];
-                    Node toNode = (Node)graph.nodes[nodeIds[i - 1]];
-
-                    double distance = Functions.CalculateDistanceBetweenCoordinates(fromNode.lat, fromNode.lon, toNode.lat, toNode.lon);
-                    int speed = 50;
-                    if (way.tags.ContainsKey("highway") && speeds.ContainsKey((string)way.tags["highway"]))
-                        speed = (int)speeds[(string)way.tags["highway"]];
-                    if (way.tags.ContainsKey("maxspeed"))
-                        try
-                        {
-                            speed = Convert.ToInt32((string)way.tags["maxspeed"]);
-                        }
-                        catch (FormatException)
-                        {
-                            Console.WriteLine("Maxspeed {0} not implemented", (string)way.tags["maxspeed"]);
-                        }
-                    float timeNeeded = (float)distance / speed;
-
-                    string name = "";
-                    if (way.tags.ContainsKey("ref"))
-                        name = (string)way.tags["ref"];
-                    else if (way.tags.ContainsKey("name"))
-                        name = (string)way.tags["name"];
-
-                    toNode.AddConnection(new Connection(distance, timeNeeded, fromNode, (string)way.tags["highway"], name));
-                    if (!way.tags.ContainsKey("oneway") || ((string)way.tags["oneway"]) == "no")
-                        fromNode.AddConnection(new Connection(distance, timeNeeded, toNode, (string)way.tags["highway"], name));
-                }
-            }
         }
     }
 }
