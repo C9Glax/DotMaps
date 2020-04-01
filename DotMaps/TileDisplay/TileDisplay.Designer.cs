cff7803b3f33f275517e375ed780339d282b0941
Glax <gfreak70@gmail.com>
2020-04-01 20:33:25 +0200

    Initial Commit Working

---
+++ DotMaps/TileDisplay/TileDisplay.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace DotMaps.Tiles
+{
+    partial class TileDisplay
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(84, 20);
+            this.fileToolStripMenuItem.Text = "Open Folder";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel3});
+            this.statusStrip.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip.TabIndex = 1;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.BackColor = System.Drawing.Color.White;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(49, 17);
+            this.toolStripStatusLabel1.Text = "Loaded.";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.BackColor = System.Drawing.Color.White;
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(52, 17);
+            this.toolStripStatusLabel2.Text = "X=0,Y=0";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.BackColor = System.Drawing.Color.White;
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(61, 17);
+            this.toolStripStatusLabel3.Text = "Mouse:0,0";
+            // 
+            // TileDisplay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "TileDisplay";
+            this.Text = "TileDisplay";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+    }
+}
+
---
+++ DotMaps/TileDisplay/TileDisplay.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DotMaps.Tiles
+{
+    public partial class TileDisplay : Form
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new TileDisplay());
+        }
+
+        public TileDisplay()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawTiles(string path)
+        {
+            this.SuspendLayout();
+            string[] subdirs = Directory.GetDirectories(path); //x
+            int xAmount = subdirs.Length;
+            string[] files = Directory.GetFiles(subdirs[0]);
+            int yAmount = files.Length;
+            int tileSize = Image.FromFile(files[0]).Width;
+            int totalImages = xAmount * yAmount;
+            this.toolStripStatusLabel1.Text = "TileSize: " + tileSize + " xAmount: " + xAmount + " yAmount: " + yAmount;
+            GroupBox map = new GroupBox();
+            map.Size = new Size(xAmount * tileSize, yAmount * tileSize);
+            map.Location = new Point(this.Width / 2 - (xAmount * tileSize) / 2, this.Height / 2 - (xAmount * tileSize) / 2);
+            this.Controls.Add(map);
+            for(int x = 0; x < xAmount; x++)
+            {
+                for(int y = 0; y < yAmount; y++)
+                {
+                    this.toolStripProgressBar1.Value = (x * yAmount + (y+1))*100/totalImages;
+                    PictureBox tile = new PictureBox();
+                    tile.Image = Image.FromFile(path + "\\" + x + "\\" + y + ".png");
+                    tile.Location = new Point(x*tileSize, y*tileSize);
+                    tile.Size = new Size(tileSize, tileSize);
+                    tile.MouseMove += new MouseEventHandler(tile_MouseMove);
+                    map.Controls.Add(tile);
+                }
+            }
+            toolStripStatusLabel2.Text = "X:" + map.Left + ",Y:" + map.Top;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private Point previousLocation;
+        private void tile_MouseMove(object sender, MouseEventArgs e)
+        {
+            Point currentLocation = new Point(Cursor.Position.X - this.Location.X, Cursor.Position.Y - this.Location.Y);
+            toolStripStatusLabel3.Text = "Mouse:" + currentLocation.X + "," + currentLocation.Y;
+            Cursor.Current = Cursors.Default;
+            if (e.Button == MouseButtons.Left)
+            {
+                Cursor.Current = Cursors.Hand;
+                ((PictureBox)sender).Parent.Left -= previousLocation.X - currentLocation.X;
+                ((PictureBox)sender).Parent.Top -= previousLocation.Y - currentLocation.Y;
+                toolStripStatusLabel2.Text = "X:" + ((PictureBox)sender).Parent.Left + ",Y:" + ((PictureBox)sender).Parent.Top;
+            }
+            previousLocation = currentLocation;
+            this.Invalidate();
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.DrawTiles(folderBrowserDialog.SelectedPath);
+            }
+        }
+    }
+}
