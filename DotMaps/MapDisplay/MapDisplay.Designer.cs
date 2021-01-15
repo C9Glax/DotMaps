dcc124cd4ee759fde2af8b07e41eb040a27a836a
Glax <gfreak70@gmail.com>
2021-01-15 16:29:20 +0100

    Different Method for rendering

---
+++ DotMaps/MapDisplay/MapDisplay.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace DotMaps
+{
+    partial class TileDisplay
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openosmToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.scaleSlider = new System.Windows.Forms.TrackBar();
+            this.statusStrip.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.scaleSlider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.toolStripStatusLabel3});
+            this.statusStrip.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip.TabIndex = 2;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.BackColor = System.Drawing.Color.White;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(49, 17);
+            this.toolStripStatusLabel1.Text = "Loaded.";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.BackColor = System.Drawing.Color.White;
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(61, 17);
+            this.toolStripStatusLabel2.Text = "Mouse:0,0";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.BackColor = System.Drawing.Color.Transparent;
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(82, 17);
+            this.toolStripStatusLabel3.Text = "Lat,Lng:0.0,0.0";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openosmToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openosmToolStripMenuItem
+            // 
+            this.openosmToolStripMenuItem.Name = "openosmToolStripMenuItem";
+            this.openosmToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+            this.openosmToolStripMenuItem.Text = "Open .osm ";
+            this.openosmToolStripMenuItem.Click += new System.EventHandler(this.openosmToolStripMenuItem_Click);
+            // 
+            // scaleSlider
+            // 
+            this.scaleSlider.LargeChange = 50;
+            this.scaleSlider.Location = new System.Drawing.Point(684, 380);
+            this.scaleSlider.Maximum = 500;
+            this.scaleSlider.Minimum = 50;
+            this.scaleSlider.Name = "scaleSlider";
+            this.scaleSlider.Size = new System.Drawing.Size(104, 45);
+            this.scaleSlider.SmallChange = 10;
+            this.scaleSlider.TabIndex = 4;
+            this.scaleSlider.TickFrequency = 50;
+            this.scaleSlider.Value = 100;
+            this.scaleSlider.ValueChanged += new System.EventHandler(this.scaleSlider_ValueChanged);
+            // 
+            // TileDisplay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.scaleSlider);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "TileDisplay";
+            this.Text = "TileDisplay";
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.scaleSlider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openosmToolStripMenuItem;
+        private System.Windows.Forms.TrackBar scaleSlider;
+    }
+}
+
---
+++ DotMaps/MapDisplay/MapDisplay.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+using DotMaps.Datastructures;
+
+namespace DotMaps
+{
+    public partial class TileDisplay : Form
+    {
+        public TileDisplay()
+        {
+            InitializeComponent();
+            this.scale = scaleSlider.Value;
+        }
+
+        private int scale;
+        private Graph mapGraph;
+        private _3DNode renderCenter;
+
+        private void openosmToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog
+            {
+                FileName = "map.osm",
+                DefaultExt = ".osm",
+                Filter = "Open Street Map (.osm)|*.osm|All files (*.*)|*.*",
+                InitialDirectory = Environment.CurrentDirectory,
+                RestoreDirectory = true
+            };
+            if(fd.ShowDialog() == DialogResult.OK)
+            {
+                this.mapGraph = Utils.Importer.ImportOSM(fd.FileName);
+                this.renderCenter = new _3DNode(this.mapGraph.minLat + (this.mapGraph.maxLat - this.mapGraph.minLat) / 2, this.mapGraph.minLon + (this.mapGraph.maxLon - this.mapGraph.minLon) / 2);
+            }
+        }
+
+        private void DrawMap()
+        {
+            
+        }
+
+        private void scaleSlider_ValueChanged(object sender, EventArgs e)
+        {
+            this.scale = scaleSlider.Value;
+            //Draw
+
+        }
+    }
+}
